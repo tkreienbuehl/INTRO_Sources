b6ea7b2aaeae7540cb411a39f62e2295a10f2a00
Tobias Kreienbühl <tobias@localhost.localdomain>
2016-10-13 15:58:01 +0200

    Neu Critical Sections nach Musterlösung

--- Project_Common/Event.c
+++ Project_Common/Event.c
@@ -29,36 +29,40 @@ static EVNT_MemUnit EVNT_Events[((EVNT_NOF_EVENTS-1)/EVNT_MEM_UNIT_NOF_BITS)+1];
 
 void EVNT_SetEvent(EVNT_Handle event) {
   /* \todo Make it reentrant */
-	EnterCritical();
+	CS1_CriticalVariable()
+	CS1_EnterCritical();
 	SET_EVENT(event);
-	ExitCritical();
+	CS1_ExitCritical();
 }
 
 void EVNT_ClearEvent(EVNT_Handle event) {
   /* \todo Make it reentrant */
-	EnterCritical();
+	CS1_CriticalVariable()
+	CS1_EnterCritical();
 	CLR_EVENT(event);
-	ExitCritical();
+	CS1_ExitCritical();
 }
 
 bool EVNT_EventIsSet(EVNT_Handle event) {
   /* \todo Make it reentrant */
 	bool retVal;
-	EnterCritical();
+	CS1_CriticalVariable()
+	CS1_EnterCritical();
 	retVal = GET_EVENT(event);
-	ExitCritical();
+	CS1_ExitCritical();
 	return retVal;
 }
 
 bool EVNT_EventIsSetAutoClear(EVNT_Handle event) {
   bool res;
   /* \todo Make it reentrant */
-  EnterCritical();
+  CS1_CriticalVariable()
+  CS1_EnterCritical();
   res = GET_EVENT(event);
   if (res) {
     CLR_EVENT(event); /* automatically clear event */
   }
-  ExitCritical();
+  CS1_ExitCritical();
   return res;
 }
 
@@ -66,7 +70,8 @@ void EVNT_HandleEvent(void (*callback)(EVNT_Handle), bool clearEvent) {
    /* Handle the one with the highest priority. Zero is the event with the highest priority. */
    EVNT_Handle event;
    /* \todo Make it reentrant */
-   EnterCritical();
+   CS1_CriticalVariable()
+   CS1_EnterCritical();
    for (event=(EVNT_Handle)0; event<EVNT_NOF_EVENTS; event++) { /* does a test on every event */
      if (GET_EVENT(event)) { /* event present? */
        if (clearEvent) {
@@ -75,7 +80,7 @@ void EVNT_HandleEvent(void (*callback)(EVNT_Handle), bool clearEvent) {
        break; /* get out of loop */
      }
    }
-   ExitCritical();
+   CS1_ExitCritical();
    if (event != EVNT_NOF_EVENTS) {
      callback(event);
      /* Note: if the callback sets the event, we will get out of the loop.
