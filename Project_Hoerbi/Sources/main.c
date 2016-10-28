3efc05e7c0c6d9e21685e0c43e2c9b3444446339
SilvanRitz <silvan.ritz@stud.hslu.ch>
2016-10-28 14:50:50 +0200

    Added runtime statistics for Remote and Robo changed pe file

--- Project_Hoerbi/Sources/main.c
+++ Project_Hoerbi/Sources/main.c
@@ -39,6 +39,8 @@
 #include "SW1.h"
 #include "BitIoLdd3.h"
 #include "CLS1.h"
+#include "FRTOS1.h"
+#include "RTOSCNTRLDD1.h"
 #include "TU1.h"
 #include "TI1.h"
 #include "TimerIntLdd1.h"
--- Project_Remote/Sources/main.c
+++ Project_Remote/Sources/main.c
@@ -30,6 +30,7 @@
 /* Including needed modules to compile this module/procedure */
 #include "Cpu.h"
 #include "Events.h"
+#include "FRTOS1.h"
 #include "LEDPin1.h"
 #include "BitIoLdd17.h"
 #include "SW1.h"
