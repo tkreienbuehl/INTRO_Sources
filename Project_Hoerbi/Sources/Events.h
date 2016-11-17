ab45d4936c4db6d8ee0d4d8134eb0d996ad63754
SilvanRitz <silvan.ritz@stud.hslu.ch>
2016-11-17 16:30:37 +0100

    Robo 1 runns proper forward

--- Project_Hoerbi/Sources/Events.h
+++ Project_Hoerbi/Sources/Events.h
@@ -65,6 +65,28 @@
 #include "BitIoLdd10.h"
 #include "IR6.h"
 #include "BitIoLdd11.h"
+#include "Q4CLeft.h"
+#include "C12.h"
+#include "BitIoLdd16.h"
+#include "C23.h"
+#include "BitIoLdd17.h"
+#include "Q4CRight.h"
+#include "C13.h"
+#include "BitIoLdd18.h"
+#include "C25.h"
+#include "BitIoLdd19.h"
+#include "MOTTU.h"
+#include "DIRL.h"
+#include "BitIoLdd12.h"
+#include "PWMR.h"
+#include "PwmLdd2.h"
+#include "DIRR.h"
+#include "BitIoLdd13.h"
+#include "PWML.h"
+#include "PwmLdd3.h"
+#include "QuadInt.h"
+#include "TimerIntLdd2.h"
+#include "TU_QuadInt.h"
 #include "KIN1.h"
 #include "IFsh1.h"
 #include "IntFlashLdd1.h"
@@ -176,6 +198,22 @@ void FRTOS1_vApplicationIdleHook(void);
 **     Returns     : Nothing
 ** ===================================================================
 */
+
+/*
+** ===================================================================
+**     Event       :  QuadInt_OnInterrupt (module Events)
+**
+**     Component   :  QuadInt [TimerInt]
+**     Description :
+**         When a timer interrupt occurs this event is called (only
+**         when the component is enabled - <Enable> and the events are
+**         enabled - <EnableEvent>). This event is enabled only if a
+**         <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void QuadInt_OnInterrupt(void);
 
 /* END Events */
 
